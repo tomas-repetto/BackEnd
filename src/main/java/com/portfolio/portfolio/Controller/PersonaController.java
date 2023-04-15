64c8fe73c85d2abed45296bc09abccbeda269506
Tomás Repetto <tomyrep04@gmail.com>
2023-04-15 12:40:43 -0300

    Clonado de BackEnd

---
+++ src/main/java/com/portfolio/portfolio/Controller/PersonaController.java
@@ -0,0 +1,64 @@
+package com.portfolio.portfolio.Controller;
+
+import com.portfolio.portfolio.Entity.Persona;
+import com.portfolio.portfolio.Interface.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class PersonaController {
+    @Autowired IPersonaService ipersonaService;
+    
+    
+    @GetMapping("/personas/traer")
+    public List<Persona> getPersona(){
+        return ipersonaService.getPersona();
+    }
+    
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/personas/crear")
+    public String createPersona(@RequestBody Persona persona){
+        ipersonaService.savePersona(persona);
+        return "La persona fue creada.";
+        
+    }
+    
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/personas/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        ipersonaService.deletePersona(id);
+        return "La persona fue borrada";
+    }
+    
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/personas/editar/{id}")
+    public Persona editPersona (@PathVariable Long id,
+            @RequestParam("nombre") String muevoNombre,
+            @RequestParam("apellido") String muevoApellido,
+            @RequestParam("img") String muevoImg){
+        Persona persona = ipersonaService.findPersona(id);
+        
+        persona.setNombre(muevoNombre);
+        persona.setApellido(muevoApellido);
+        persona.setImg(muevoImg);
+        
+        ipersonaService.savePersona(persona);
+        return persona;
+    }
+    
+    @GetMapping("/personas/traer/pefil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)2);
+    }
+}
---
+++ src/main/java/com/portfolio/portfolio/Security/Service/UsuarioService.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.portfolio.Security.Service;
+
+import com.portfolio.portfolio.Security.Entity.Usuario;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.portfolio.portfolio.Security.Repository.iUsuarioRepository;
+
+/**
+ *
+ * @author tomyr
+ */
+
+@Service
+@Transactional
+public class UsuarioService {
+    @Autowired
+    iUsuarioRepository iusuarioRepository;
+    
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
+        return iusuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+    
+    public boolean existByNombreUsuario(String nombreUsuario){
+        return iusuarioRepository.existsByNombreUsuario(nombreUsuario);
+    }
+    
+    public boolean existByEmail(String email){
+        return iusuarioRepository.existsByEmail(email);
+    }
+    
+    public void save(Usuario usuario){
+        iusuarioRepository.save(usuario);
+    }
+}
