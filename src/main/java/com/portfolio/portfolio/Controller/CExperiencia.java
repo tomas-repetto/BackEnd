a1e70e3c5e56e239c7768d4c96441a4c8c91afe1
Tomás Repetto <tomyrep04@gmail.com>
2023-04-30 23:03:22 -0300

    crossorigin cambio

--- src/main/java/com/portfolio/portfolio/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/portfolio/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://argentinaprogramafronten-9e640.web.app/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
